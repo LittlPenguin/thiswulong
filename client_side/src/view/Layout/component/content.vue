22685b5d3b36a7583b08422d1aae37205df8132d
Aya <3121498325@qq.com>
2025-10-25 14:06:02 +0800

    修复bug 添加了内容去上层黏性的样式

--- client_side/src/view/Layout/component/content.vue
+++ client_side/src/view/Layout/component/content.vue
@@ -4,110 +4,108 @@ import { ArrowBigLeft, ArrowBigRight, Music, Link } from "@vicons/tabler";
 </script>
 
 <template>
-  <n-layout
-    class="right-content"
-    :native-scrollbar="false"
-    style="background-color: transparent"
-  >
-    <n-layout style="background-color: transparent">
-      <n-layout-header class="content-top">
-        <n-flex justify="space-between" style="height: 100%">
-          <ul>
-            <li>
-              <n-button secondary strong class="apple-icon">
-                <template #icon>
-                  <NIcon>
-                    <ArrowBigLeft />
-                  </NIcon>
-                </template>
-              </n-button>
-            </li>
-            <li>
-              <n-button secondary strong class="apple-icon">
-                <template #icon>
-                  <NIcon>
-                    <ArrowBigRight />
-                  </NIcon>
-                </template>
-              </n-button>
-            </li>
-            <li>
-              <n-button secondary strong class="apple-icon">
-                <template #icon>
-                  <NIcon>
-                    <Music />
-                  </NIcon>
-                </template>
-              </n-button>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <n-button secondary strong class="apple-icon">
-                <template #icon>
-                  <NIcon>
-                    <Link />
-                  </NIcon>
-                </template>
-              </n-button>
-            </li>
-            <li class="apple-icon">
-              <n-ellipsis style="max-width: 240px">
-                住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光
-                停滞的海浪
-              </n-ellipsis>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <n-tag round :bordered="false" type="success">
-                IsOk
-                <template #icon>
-                  <n-icon :component="CheckmarkCircle" />
-                </template>
-              </n-tag>
-            </li>
-          </ul>
-        </n-flex>
-      </n-layout-header>
-      <n-layout-content style="background-color: #e3e3e3">
-        <div class="ContentContainer">
-          <n-layout></n-layout>
-        </div>
-      </n-layout-content>
-      <n-layout-footer>成府路</n-layout-footer>
+  <n-layout style="margin: 5px"  :native-scrollbar="false"  class="ContentLayout">
+    <div class="content-top">
+      <n-flex justify="space-between" style="height: 100%">
+        <ul>
+          <li>
+            <n-button secondary strong class="apple-icon">
+              <template #icon>
+                <NIcon>
+                  <ArrowBigLeft />
+                </NIcon>
+              </template>
+            </n-button>
+          </li>
+          <li>
+            <n-button secondary strong class="apple-icon">
+              <template #icon>
+                <NIcon>
+                  <ArrowBigRight />
+                </NIcon>
+              </template>
+            </n-button>
+          </li>
+          <li>
+            <n-button secondary strong class="apple-icon">
+              <template #icon>
+                <NIcon>
+                  <Music />
+                </NIcon>
+              </template>
+            </n-button>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <n-button secondary strong class="apple-icon">
+              <template #icon>
+                <NIcon>
+                  <Link />
+                </NIcon>
+              </template>
+            </n-button>
+          </li>
+          <li class="apple-icon">
+            <n-ellipsis style="max-width: 240px">
+              住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪
+            </n-ellipsis>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <n-tag round :bordered="false" type="success">
+              IsOk
+              <template #icon>
+                <n-icon :component="CheckmarkCircle" />
+              </template>
+            </n-tag>
+          </li>
+        </ul>
+      </n-flex>
+    </div>
+    <n-layout
+      style="background-color: #e3e3e3"
+      class="ContentContainer"
+      :native-scrollbar="false"
+    >
     </n-layout>
   </n-layout>
 </template>
 
 <style scoped lang="scss">
-.right-content {
-  margin: 5px;
+.ContentLayout {
   border-radius: 5px;
-  & .content-top {
-    height: 30px;
-    background-color: #fff;
-    & ul {
-      list-style: none;
+}
+.content-top {
+  position: sticky;
+  width: 100%;
+  top: 0;
+  z-index: 999;
+  background-color: #fff;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+  & ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0 10px;
+    & .apple-icon {
       display: flex;
       justify-content: center;
-      margin: 0;
-      padding: 0 10px;
-      & .apple-icon {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: transparent;
-        transition: color 0.3s ease;
-        color: #898989;
-        &:hover {
-          color: #515151;
-        }
+      align-items: center;
+      background-color: transparent;
+      transition: color 0.3s ease;
+      color: #898989;
+      &:hover {
+        color: #515151;
       }
     }
   }
 }
+
 .ContentContainer {
-  min-height: 100vh;
+  min-height: 95vh;
 }
 </style>
